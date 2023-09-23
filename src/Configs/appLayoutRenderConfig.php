322be3f56743b7359340036f7d4ac2d6034dd5a4
brit45 <36789485+brit45@users.noreply.github.com>
2023-09-23 23:57:19 +0200

    MOD ( fix cache directory )

--- src/Configs/appLayoutRenderConfig.php
+++ src/Configs/appLayoutRenderConfig.php
@@ -6,7 +6,7 @@ namespace Root22\Router\Config;
 return [
 
     // Blade Configuration
-    'cache_dir' => ROOT.'/src/tmp',
+    'cache_dir' => ROOT.'/src/cache/tmp/blade',
 
     // Directory of Templates.
     'views_dir' => ROOT.'/src/Views'
